cec6dd58e076d0302af3146e6b6ae87aa51b4c3c
YASSA Sandro OBS/OCB <sandro.yassa@orange.com>
2022-03-21 00:03:27 +0200

    serving resized/cached file

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,19 +1,40 @@
 import express from 'express';
 import settings from '../../settings';
 import path from 'path';
+import { constants as fsConstants, promises as fsPromises } from 'fs';
+import sharp from 'sharp';
 
 const image = express.Router();
 
-image.get('/', (req, res) => {
-  const filename = req.query.filename as string;
-  const filepath = path.join(
-    settings.BASE_DIR,
-    'media',
-    'originals',
-    filename + '.jpg'
-  );
+image.get('/', async (req, res) => {
+  const filename = (req.query.filename as string) + '.jpg';
+  const fileorigin = path.join('media', 'originals', filename);
+  const height = req.query.height as string | undefined;
+  const width = req.query.width as string | undefined;
+  const filedir = path.join('media', `${width}x${height}`);
 
-  res.sendFile(filepath);
+  // Serve original
+  if (!(height && width)) {
+    return res.sendFile(path.join(settings.BASE_DIR, fileorigin));
+  }
+
+  // Create new dir by dimensions if needed
+  try {
+    await fsPromises.access(filedir, fsConstants.W_OK);
+  } catch (error) {
+    await fsPromises.mkdir(filedir);
+  }
+
+  // Resize file if needed
+  try {
+    await fsPromises.access(path.join(filedir, filename), fsConstants.R_OK);
+  } catch (error) {
+    await sharp(fileorigin)
+      .resize(parseInt(width), parseInt(height), { fit: 'contain' })
+      .toFile(path.join(filedir, filename));
+  }
+
+  res.sendFile(path.join(settings.BASE_DIR, filedir, filename));
 });
 
 export default image;
